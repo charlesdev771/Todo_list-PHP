ea6eef78e35a8d77be64a9b587d02f3db36ed6f3
charlesdev771 <charlesdev0721@protonmail.com>
2022-07-17 16:48:17 -0300

    First

---
+++ lista_tarefas/conexao.php
@@ -0,0 +1,20 @@
+<?php
+
+    private $host = 'localhost';
+    private $dbname = 'php_com_pdo';
+    private $user = 'root';
+    private $pass = '';
+
+    public function conectar()
+    {
+        try 
+        {
+        
+        }catch (\Throwable $th) 
+        {
+        
+        }
+    }
+
+
+?>
---
+++ lista_tarefas/tarefa.service.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class TarefaService
+{
+    public function inserir()
+    {
+
+    }  
+
+    public function recuperar()
+    {
+
+    }
+
+    public function atualizar()
+    {
+
+    }
+
+    public function remover()
+    {
+
+    }
+}
+
+?>
